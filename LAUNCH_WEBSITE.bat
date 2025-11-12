d52c28755391bec85bb4b46bdfadf21a8b3ecabc
Ali Hassan <alihassan.pk019@gmail.com>
2025-11-12 22:19:19 +0100

    Final

---
+++ LAUNCH_WEBSITE.bat
@@ -0,0 +1,21 @@
+@echo off
+echo.
+echo ========================================
+echo   CityLocal 101 Clone - Launcher
+echo ========================================
+echo.
+echo Opening website in your default browser...
+echo.
+
+start "" "index.html"
+
+echo.
+echo Website opened!
+echo.
+echo If the website didn't open, try:
+echo 1. Double-click "index.html"
+echo 2. Right-click index.html and "Open with Browser"
+echo.
+echo Press any key to close this window...
+pause >nul
+
